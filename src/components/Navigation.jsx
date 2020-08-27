d5db828aa2f16b1382b4fff5b2d87c19694a5cdb
Tom <demagny.t@hotmail.fr>
2020-08-27 16:44:24 +0200

    modify Link to NavLink

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,29 +1,29 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navigation(){
 
     return (
         <React.Fragment>
             <nav className="pt-4 pb-4 bg-secondary ">
-                <Link to="/">
+                <NavLink to="/">
                     <button className="btn btn-dark ml-3">
                         Home
                     </button>
-                </Link>
-                <Link to="/Authors">
+                </NavLink>
+                <NavLink to="/Authors">
                     <button
                         className="btn btn-dark ml-3">
                         Auteurs
                     </button>
-                </Link>
-                <Link to="/Admin">
+                </NavLink>
+                <NavLink to="/Admin">
                     <button
                         className="btn btn-dark ml-3">
                         Auteurs Admin
                     </button>
-                </Link>
+                </NavLink>
 
             </nav>
         </React.Fragment>)
