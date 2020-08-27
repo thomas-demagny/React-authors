1668da28b7f5f8fba29835dbce335704f50e1787
Tom <demagny.t@hotmail.fr>
2020-08-27 16:42:31 +0200

    modify index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Provider from "./Provider";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+      <Provider>
     <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
