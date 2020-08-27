1316d8b1738a5030c3392b53f76238d31f0a75c6
Tom <demagny.t@hotmail.fr>
2020-08-27 16:44:47 +0200

    text modify

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Home() {
+    return(
+        <React.Fragment>
+            <h1 className="text-center pb-4"> Les supers auteurs </h1>
+            <div className="container text-left">
+                <p>Bonjour bienvenu sur la liste des auteurs.</p>
+                <p>Ici vous pourrez noter vos auteurs préférés suivant leurs passions.</p>
+                <p>Amusez vous bien !</p>
+            </div>
+
+        </React.Fragment>
+    )
+}
+
+export default Home;
+
