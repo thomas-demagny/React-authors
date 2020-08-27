7fbc02c433ca072145097098411b225455d76213
Tom <demagny.t@hotmail.fr>
2020-08-27 11:59:04 +0200

    add Admin & authors files

---
+++ src/components/Admin.jsx
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Admin() {
+    return(
+        <React.Fragment>
+            <h1 className="text-center pb-4"> Admin des auteurs </h1>
+            <div className="container text-left">
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores assumenda beatae commodi
+                    earum enim eum expedita fuga, itaque libero maiores modi officia quam qui quibusdam quod ratione
+                    sapiente. Ea!
+                </div>
+                <div>Architecto, esse in? Ab ad aliquam animi consequuntur cumque doloremque dolores dolorum eum
+                    excepturi hic illo illum in incidunt itaque iusto maxime modi neque provident quos sit tempora,
+                    voluptas voluptatum!
+                </div>
+                <div>Atque commodi distinctio, dolor ducimus esse eveniet expedita, id illo illum iste magnam mollitia
+                    omnis, perspiciatis provident quis ratione tenetur totam vel veniam veritatis? Amet consectetur
+                    obcaecati officiis perferendis quo?
+                </div>
+            </div>
+
+            <p><i className="fa fa-angle-double-left"> Retour à l'accueil</i></p>
+        </React.Fragment>
+    )
+}
+
+export default Admin;
---
+++ src/components/Authors.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Authors() {
+    return(
+        <React.Fragment>
+            <h1 className="text-center pb-4"> Les auteurs et leurs passions</h1>
+            <div className="container text-left">
+                <p>liste des auteurs</p>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Authors;
