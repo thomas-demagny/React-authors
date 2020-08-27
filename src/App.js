0f8835d8bb858d774991145c40cafd7ff0d8db60
Tom <demagny.t@hotmail.fr>
2020-08-27 11:02:52 +0200

    update : add imports nav + home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <Navigation />
+        <div className="text-center pt-3">
+         <Home />
+        </div>
+
+      </div>
   );
 }
 
 export default App;
+
+
+
+
+
