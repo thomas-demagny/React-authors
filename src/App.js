b10145d7bd66abc36d007b82fc8c01d0accea5eb
Tom <demagny.t@hotmail.fr>
2020-08-27 16:43:09 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Authors from './components/Authors';
+import Admin from './components/Admin';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 
 function App() {
-  return (
-      <div className="App">
-          <Navigation />
-        <div className="text-center pt-3">
-         <Home />
-        </div>
 
-      </div>
-  );
+
+    return (
+        <div className="App">
+            <Router>
+                <Navigation />
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/authors" component={Authors}/>
+                    <Route exact path="/admin" component={Admin}/>
+                    <Route
+                        path="/"
+                        component={() =>
+                            <p>Oups cette page n'existe pas</p>}
+                    />
+
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
+
+
+
+
 
 
 
