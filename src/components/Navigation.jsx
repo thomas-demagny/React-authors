53d87cab3a8d12a7ccfff8e2714455d255981291
Tom <demagny.t@hotmail.fr>
2020-08-27 10:31:54 +0200

    rename and add button list for nav

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Navigation(){
+
+    return (
+        <React.Fragment>
+            <nav className="pt-4 pb-4 bg-secondary ">
+                <button className="btn btn-dark ml-3">Home</button>
+                <button className="btn btn-dark ml-3">Auteurs</button>
+                <button className="btn btn-dark ml-3">Auteurs Admin</button>
+            </nav>
+        </React.Fragment>)
+
+}
+
+export default Navigation;
