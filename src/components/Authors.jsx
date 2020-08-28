2390889e560547f603cc847a77302a566224bb55
Tom <demagny.t@hotmail.fr>
2020-08-28 11:29:22 +0200

    add notes

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -8,11 +8,22 @@ function Authors() {
 
     const history = useHistory();
 
-    const [state] = useReducer(reducer, initialState);
-
-    const {authors} = state;
+    const [state, dispatch] = useReducer(reducer, initialState);
 
 
+
+    const handleChange = e => {
+        const { value , name } = e.target;
+        if (name === 'note')
+        dispatch({ type: "SET_NOTE", note: value });
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        dispatch({ type: "ADD_NOTE" });
+    }
+    const {authors, note,message} = state;
     return(
         <React.Fragment>
             <h1 className="text-center pb-4"> Les auteurs et leurs passions</h1>
@@ -24,7 +35,9 @@ function Authors() {
                     <tr className="bg-secondary">
                         <th scope="col">Auteur</th>
                         <th scope="col">Passion</th>
-                        <th scope="col">Notation</th>
+                        <th scope="col">Notes reçues</th>
+                        <th scope="col">Votre notation</th>
+                        <th scope="col">Moyenne des notes</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -32,7 +45,14 @@ function Authors() {
                         <tr key={i}>
                             <td>{author.name}</td>
                             <td>{(author.passion).join(' - ')}</td>
-                            <td>Pas de notes pour l'instant</td>
+                            <td>{author.notes.join(' - ')}</td>
+                            <td><form onClick={handleSubmit}>
+                                {message !== '' && <p>{message}</p>}
+
+                                <input type="text" name='note' value={note} onChange={handleChange}/>
+                                <button className="btn btn-outline-info">Ajouter</button>
+                            </form></td>
+                            <td>Pas encore de moyenne</td>
                         </tr>
                     )}
                     </tbody>
@@ -42,7 +62,7 @@ function Authors() {
                 </div>
             </div>
 
-            <button className="d-flex align-content btn btn-info fa fa-angle-double-left " onClick={ () => history.push('/')}> Retour à l'accueil</button>
+            <button className="btn btn-info fa fa-angle-double-left " onClick={ () => history.push('/')}> Retour à l'accueil</button>
         </React.Fragment>
     )
 }
